bf05af0eeb70d8c3bd80f1b4cbc75dee819d591a
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2019-09-19 18:13:07 +0530

    Update Joe.java

--- udpcommunication/src/udpcommunication/Joe.java
+++ udpcommunication/src/udpcommunication/Joe.java
@@ -1,5 +1,6 @@
 package udpcommunication;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -8,20 +9,101 @@ import java.util.Scanner;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class RecieveJoe extends Thread
 {
+	Buffer o=new Buffer();
+	String message[];
 	
-	public void start()
-	{
-		
-		
-	}
 	
 	public void run()
-	{
+	{	
+		DatagramSocket joeSocket=null;
+		//DatagramSocket marieSocket=null;
+		byte[] joereceive = new byte[65535]; 
+		//byte[] mariereceive = new byte[65535]; 
+		DatagramPacket JoeDpReceive = null; 
+		//DatagramPacket MarieDpReceive = null; 
+		try {
+		
+		joeSocket=new DatagramSocket(1235);
+		
+	//	marieSocket=new DatagramSocket(1235);
+		
+		
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		while (true)
+	     { 
+	     
+	         
+	  //       MarieDpReceive = new DatagramPacket(mariereceive, mariereceive.length); 
+	    
+			
+			JoeDpReceive = new DatagramPacket(joereceive, joereceive.length); 
+
+	        
+	         try
+	         {
+	        	 
+	      //System.out.println();   
+	         
+	     joeSocket.receive(JoeDpReceive); 
+	         }catch(Exception e) {e.printStackTrace();}
+	         
+	         System.out.println("\t\t\t" + data(joereceive) ); 
+	         
+
+	         joereceive = new byte[65535];
+	        
+	     
+	     
+	     } 
+		
+		
+		
+		
+		
 		
 		
 	}
+	
+	
+	
+	
+	public static StringBuilder data(byte[] a) 
+	 { 
+	     if (a == null) 
+	         return null; 
+	     StringBuilder ret = new StringBuilder(); 
+	     int i = 0; 
+	     while (a[i] != 0) 
+	     { 
+	         ret.append((char) a[i]); 
+	         i++; 
+	     } 
+	     return ret; 
+	 } 
+	
+	
 	
 	
 }
@@ -29,11 +111,7 @@ class RecieveJoe extends Thread
 class SendJoe extends Thread
 {
 	
-	public void init()throws Exception
-	{
-		 
-		 
-	}
+	
 	
 	public void run()
 	{
@@ -57,10 +135,15 @@ class SendJoe extends Thread
 		        
 		        while (true) 
 		        { 
+		        	
+		        	
+		        	
+		        	
+		        	
 		        	sc = new Scanner(System.in); 
-		        	System.out.println("enter text :   ");
+		        	System.out.println("");
 		            String inp = sc.nextLine(); 
-		  
+		        	//String inp="";
 		            buf = inp.getBytes(); 
 		  
 		          
@@ -70,6 +153,7 @@ class SendJoe extends Thread
 		            try {
 		            ds.send(DpSend); 
 		            	}catch(Exception e) {}
+		
 		           
 		        } 
 		
@@ -85,9 +169,9 @@ public class Joe {
 	
 	public static void main(String[] args)throws Exception {
 	
-		SendJoe s=new SendJoe();
-		s.start();
-		s.join();
+		SendJoe s=new SendJoe();RecieveJoe r=new RecieveJoe();
+		s.start();r.start();
+		
 		
 		
 	}
@@ -102,26 +186,3 @@ public class Joe {
 		        
 		    
 	
-	
-	
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
