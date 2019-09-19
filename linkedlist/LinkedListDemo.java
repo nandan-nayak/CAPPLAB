63de0e30f3712f964536c97646384e45d74869a4
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2019-09-19 15:14:35 +0530

    Update LinkedListDemo.java

--- linkedlist/LinkedListDemo.java
+++ linkedlist/LinkedListDemo.java
@@ -81,60 +81,104 @@ public class LinkedListDemo {
 		
 		
 		LinkedList list=new LinkedList();
-		int choice=0,data=0;
+		int choice=0,data=0,size=0,i;
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
 		do
 		{
-			
+			System.out.println("\n\n\n=======================================");
 			System.out.println("1:add element to head");
 			System.out.println("2:delete element from head ");
 			System.out.println("3:view content");
 			System.out.println("4:move last element to first");
 			System.out.println("5:exit");
-			System.out.println("enter your choice : ");
+			System.out.println("=======================================");
+			System.out.print("enter your choice : ");
+			//System.out.println("\n\n");
+			
+
+
 			try {
 			choice=Integer.parseInt(in.readLine());
 			}catch(IOException e)
 			{
 				e.printStackTrace();
 			}
+
+
+
 			switch(choice)
 			{
+
+
+
 			case 1:
-				System.out.println("============================");
-				System.out.println("\n\n enter the data ");
+
+
+
+				
+				System.out.print("\n\n enter the size : ");
 				try {
-					data=Integer.parseInt(in.readLine());
+					size=Integer.parseInt(in.readLine());
 					}catch(IOException e)
 					{
 						e.printStackTrace();
 					}
+				for(i=0;i<size;i++)	
+					{	
+					System.out.print("enter data "+i +" : ");	
+					try {
+					data=Integer.parseInt(in.readLine());
+					}catch(IOException e)
+					{
+						e.printStackTrace();
+					}		
+					list=list.addNode(list, data);
+					}				
+
+
 				
-				list=list.addNode(list, data);
-				System.out.println("============================");
 				break;
-			case 2:System.out.println("============================\n");
-			System.out.println("deleted successfully");
+
+
+
+
+
+
+			case 2:
+				System.out.println("\n\n");
+				System.out.println("deleted successfully");
 				list=list.deleteNode(list);
-				System.out.println("============================");
+				
 				break;
 				
 				
+
+
 			case 3:
-				System.out.println("============================\n");
+
+
 				
+				System.out.println("\n\n");
 				list.display(list);
-				System.out.println("============================");
+				
 				break;
+
+
+
+
 			case 4:
-				System.out.println("============================\n");
+				System.out.println("\n\n");
 				list=list.move(list);
 				System.out.println(" moved successfully ");
-				System.out.println("============================");
+				
 				
 				
 				break;
+
+
+
+
 			case 5:System.exit(0);
 			
 			
